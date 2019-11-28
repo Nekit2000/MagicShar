e77747e830ae0a0c60fc6b61c4cf2d90278008d0
Felaveq <46960577+Felaveq@users.noreply.github.com>
2019-11-28 12:53:29 +0500

    Разработка пользовательского интерфейса от Никиты.

---
+++ MagicBall/MagicBall/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MagicBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(213, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 410);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(436, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Я не знаю...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введи свой вопрос";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Узнать ответ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(24, 75, 24, 25);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задай свой вопрос";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MagicBall/MagicBall/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagicBall
+{
+   
+    public partial class Form1 : Form
+    {
+        public string GetAnswer(TextBox text)
+        {
+            Random rnd = new Random();
+            //Выбираем ответ
+            int answer_number = rnd.Next(1, 6);
+            // Логика ответа
+            // Сколько, где, когда, зачем, почему, как, обобщающий вопрос
+            string newtext = text.Text.ToLower();
+            string[] words = newtext.Split(new char[] { ' ' });
+
+            if (words.Contains("сколько"))
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Большое количество";
+                    case 2:
+                        return "Совсем немного";
+                    case 3:
+                        return "Мало";
+                    case 4:
+                        return "Нисколько";
+                    case 5:
+                        return "Огромное количество";
+                    case 6:
+                        return "Много";
+                }
+            }
+            else if (words.Contains("где"))
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Нигде";
+                    case 2:
+                        return "В далеких землях";
+                    case 3:
+                        return "Близко";
+                    case 4:
+                        return "Совсем рядом";
+                    case 5:
+                        return "Под землей";
+                    case 6:
+                        return "Выше облаков";
+                }
+            }
+            else if (words.Contains("когда"))
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Скоро";
+                    case 2:
+                        return "Никогда";
+                    case 3:
+                        return "Завтра";
+                    case 4:
+                        return "В ближайшее время";
+                    case 5:
+                        return "Нескоро";
+                    case 6:
+                        return "Совсем скоро";
+                }
+            }
+            else if (words.Contains("зачем"))
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Затем";
+                    case 2:
+                        return "Просто так";
+                    case 3:
+                        return "Такая судьба";
+                    case 4:
+                        return "Так получилось";
+                    case 5:
+                        return "За мясом :)";
+                    case 6:
+                        return "Так надо";
+                }
+            }
+            else if (words.Contains("почему"))
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Потому";
+                    case 2:
+                        return "Покачану";
+                    case 3:
+                        return "Так надо";
+                    case 4:
+                        return "Это было неправильно";
+                    case 5:
+                        return "Это было правильно";
+                    case 6:
+                        return "Это важно";
+                }
+            }
+            else if (words.Contains("как"))
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Отлично";
+                    case 2:
+                        return "Нелегко";
+                    case 3:
+                        return "Вот так";
+                    case 4:
+                        return "Сложно";
+                    case 5:
+                        return "Непросто";
+                    case 6:
+                        return "Легко";
+                }
+            }
+            else
+            {
+                switch (answer_number)
+                {
+                    case 1:
+                        return "Сложный вопрос";
+                    case 2:
+                        return "Надо подумать";
+                    case 3:
+                        return "Да";
+                    case 4:
+                        return "Нет";
+                    case 5:
+                        return "Скорее да";
+                    case 6:
+                        return "Скорее нет";
+                }
+            }
+            
+
+            return "s";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = GetAnswer(textBox1);
+        }
+    }
+}
